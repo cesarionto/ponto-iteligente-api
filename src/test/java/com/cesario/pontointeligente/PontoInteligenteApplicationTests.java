3f1f5e489cb4dd20273a7ce3931cbe1a0ac9c9a8
cesarionto <cesariopereiraneto@gmail.com>
2020-11-10 17:07:42 -0300

    Commit de uploud do projeto

---
+++ src/test/java/com/cesario/pontointeligente/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesario.pontointeligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
